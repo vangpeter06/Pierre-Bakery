b9c219f37f5b0ce0a198a56ff00a15ab952e2328
Peter Vang <vang_peter06@yahoo.com>
2022-07-15 09:01:14 -0700

    Bread Test 2 pass

--- PierreBakery.Tests/ModelTests/BreadTests.cs
+++ PierreBakery.Tests/ModelTests/BreadTests.cs
@@ -11,9 +11,22 @@ namespace PierreBakery.Tests
     [TestMethod]
     public void BreadConstructor_CreatesInstanceOfBread_Bread()
     {
-      // int quantity = 1;
-      Bread newBread = new Bread();
+      int Quantity = 1;
+      Bread newBread = new Bread(Quantity);
       Assert.AreEqual(typeof(Bread),newBread.GetType());
+    }
+
+    [TestMethod]
+    public void BreadOrder_CheckBreadOrder_Five()
+    {
+      int numBreadOrder = 1;
+      int orderAmount = 5;
+
+      int result = Bread.BreadOrder(numBreadOrder);
+
+      Assert.AreEqual(orderAmount,result);
+
+
     }
   }
 }
