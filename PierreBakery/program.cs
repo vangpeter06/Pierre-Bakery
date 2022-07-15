2741351928acd39ec0a5632955c707c73722276e
Peter Vang <vang_peter06@yahoo.com>
2022-07-15 16:28:53 -0700

    add total cost of user input and display results

--- PierreBakery/program.cs
+++ PierreBakery/program.cs
@@ -9,24 +9,31 @@ namespace PierreBakery
     static void Main()
     {
       Console.WriteLine("Welcome to Pierre Bakery");
-      Console.WriteLine("We have deals on bread buy one at $5 or 2 at $10 and get the third one free");
+      Console.WriteLine("We have deals on loaves bread buy one at $5 or 2 at $10 and get the third one free");
       Console.WriteLine("Pastry buy 1 for $2 or 3 for $5. Four pastries costs $7, five pastries costs $9, and six pastries costs $10");
       FindFinalPrice();
       static void FindFinalPrice()
       {
-        Console.WriteLine("Enter how many bread do you want?");
+        Console.WriteLine("Enter how many loaves bread do you want?");
         string userInputBread = Console.ReadLine();
         int inputBread = int.Parse(userInputBread);
         Console.WriteLine("Enter how many pastry do you want?");
         string userInputPastry = Console.ReadLine();
-        // int inputPastry = int.Parse(userInputPastry);Bread breadTotal = new Bread(inputBread);
-        
-        // Console.WriteLine("Your total for bread is $" + breadTotal.BreadOrder(inputBread));
-        
-        // Pastry pastryTotal = new Pastry(inputPastry);
-        // Console.WriteLine(breadTotal, pastryTotal);
+        int inputPastry = int.Parse(userInputPastry);
+
+        int orderOfBread = Bread.SpecialDeal(inputBread);
+        int orderTotal = Bread.BreadOrder(inputBread) + Pastry.PastryOrder(inputPastry);
+
+        Console.WriteLine("Your total is $" + orderTotal + ". You have order " + orderOfBread + " loaves of bread and " + inputPastry + " pastry.");
+        Console.WriteLine("Would you like to order more loaves of bread and pastry? If so enter 'Y' or 'y'. Crtl C to exit");
+        string answer = Console.ReadLine();
+        if (answer == "y" || answer == "Y")
         {
           FindFinalPrice();
+        }
+        else
+        {
+          Main();
         }
       }
     }
