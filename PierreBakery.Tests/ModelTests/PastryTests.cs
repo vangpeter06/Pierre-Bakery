a8dfbc8b8b16c7e4748768a35968265425b9b228
Peter Vang <vang_peter06@yahoo.com>
2022-07-15 10:22:42 -0700

    Pastry first test pass

--- PierreBakery.Tests/ModelTests/PastryTests.cs
+++ PierreBakery.Tests/ModelTests/PastryTests.cs
@@ -1,2 +1,19 @@
 using System;
 using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierreBakery.Models;
+
+namespace PierreBakery.Tests
+{
+  [TestClass]
+  public class PastryTests
+  {
+    [TestMethod]
+    public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
+    {
+      // int Quantity = 1;
+      Pastry newPastry = new Pastry();
+      Assert.AreEqual(typeof(Pastry),newPastry.GetType());
+    }
+  }
+}
