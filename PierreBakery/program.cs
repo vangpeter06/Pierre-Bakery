87b3178a2a68151dfab5bb9971837defd73c3181
Peter Vang <vang_peter06@yahoo.com>
2022-07-15 13:53:15 -0700

    add welcome prompt with cost of bread and pastry

--- PierreBakery/program.cs
+++ PierreBakery/program.cs
@@ -8,9 +8,16 @@ namespace PierreBakery
   {
     static void Main()
     {
+      Console.WriteLine("Welcome to Pierre Bakery");
+      Console.WriteLine("We have deals on bread buy one at $5 or 2 at $10 and get the third one free");
+      Console.WriteLine("Pastry buy 1 for $2 or 3 for $5. Four pastries costs $7, five pastries costs $9, and six pastries costs $10");
+      FindFinalPrice();
       static void FindFinalPrice()
       {
-        Console.WriteLine();
+        
+        {
+          FindFinalPrice();
+        }
       }
     }
   }
