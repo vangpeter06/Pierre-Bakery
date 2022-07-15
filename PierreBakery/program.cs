380ea9cf236668abae7e26c74c843029b2437b53
Peter Vang <vang_peter06@yahoo.com>
2022-07-15 13:38:24 -0700

    builds the project and its dependencies

---
+++ PierreBakery/program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using PierreBakery.Models;
+
+namespace PierreBakery
+{
+  class Program
+  {
+    static void Main()
+    {
+      static void FindFinalPrice()
+      {
+        Console.WriteLine();
+      }
+    }
+  }
+}
