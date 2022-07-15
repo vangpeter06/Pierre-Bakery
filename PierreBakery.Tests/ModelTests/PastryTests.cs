45283340b8f88069cfe292e2127118a3347c5ec8
Peter Vang <vang_peter06@yahoo.com>
2022-07-15 12:48:25 -0700

    Pastry test 3 pass

--- PierreBakery.Tests/ModelTests/PastryTests.cs
+++ PierreBakery.Tests/ModelTests/PastryTests.cs
@@ -11,9 +11,31 @@ namespace PierreBakery.Tests
     [TestMethod]
     public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
     {
-      // int Quantity = 1;
-      Pastry newPastry = new Pastry();
+      int Quantity = 1;
+      Pastry newPastry = new Pastry(Quantity);
       Assert.AreEqual(typeof(Pastry),newPastry.GetType());
+    }
+
+    [TestMethod]
+    public void PastryOrder_CheckPastryOrder_Two()
+    {
+      int numPastryOrder = 1;
+      int orderAmount = 2;
+      
+      int result = Pastry.PastryOrder(numPastryOrder);
+
+      Assert.AreEqual(orderAmount, result);
+    }
+
+    [TestMethod]
+    public void PastryOrder_BuyThree_Five()
+    {
+      int numPastryOrder = 3;
+      int orderAmount = 5;
+
+      int result = Pastry.PastryOrder(numPastryOrder);
+
+      Assert.AreEqual(orderAmount, result);
     }
   }
 }
