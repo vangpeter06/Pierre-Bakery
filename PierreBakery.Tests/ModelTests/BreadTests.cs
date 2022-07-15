aa7ed1c8a735f4b6d741539c8c590d954ec3be25
Peter Vang <vang_peter06@yahoo.com>
2022-07-15 08:44:00 -0700

    First Test passing

--- PierreBakery.Tests/ModelTests/BreadTests.cs
+++ PierreBakery.Tests/ModelTests/BreadTests.cs
@@ -1,2 +1,19 @@
 using System;
 using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierreBakery.Models;
+
+namespace PierreBakery.Tests
+{
+  [TestClass]
+  public class BreadTests
+  {
+    [TestMethod]
+    public void BreadConstructor_CreatesInstanceOfBread_Bread()
+    {
+      // int quantity = 1;
+      Bread newBread = new Bread();
+      Assert.AreEqual(typeof(Bread),newBread.GetType());
+    }
+  }
+}
